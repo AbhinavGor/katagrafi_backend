74ba495c3d6a3d93afbfb060a9c2a0cafbccde9b
Abhinav Gorantla <abhinav20016@gmail.com>
2021-05-23 18:01:25 +0530

    added route to for item addition to inventory

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,4 +1,6 @@
 const Inventory = require('../models/Inventory');
+const Item = require('../models/Item');
+const mongoose = require('mongoose');
 
 exports.createInventory = async (req, res) => {
     try {
@@ -20,4 +22,54 @@ exports.createInventory = async (req, res) => {
             "message": "Error occurred while making a new inventory, please try again later!"
         });
     }
+}
+
+exports.addItem =  async (req, res) => {
+  try{
+    const { item_name, item_image, quantity, item_type, allergic_ingredients } = req.body;
+    const inventoryId = req.params.inv_id;
+
+    const foundItem = await Item.findOne({ item_name: item_name });
+    const foundInv = await Inventory.findOne({ _id: mongoose.Schema.Types.ObjectId(inventoryId)});
+
+    if(foundInv){
+      if(foundItem){
+          foundInv.items.concat(foundItem._id);
+      }else{
+        const newItem = {
+          item_name, item_image, quantity, item_type, allergic_ingredients
+        }
+
+        await newItem.save();
+
+        const newIt = await Item.findOne({ item_name: item_name });
+
+        foundInv.items.concat(newIt._id);
+      }
+    }else{
+      res.status(404).send({"message": `Inventory with id ${inventoryId} not found!`});
+    }
+  }catch(e){
+    res.status(500).send(e);
+  }
+}
+
+exports.getInventoryItems = async (req, res) => {
+  const inv_id = req.params.inv_id;
+
+  try {
+
+      const foundInv = await Inventory.findOne({ _id: mongoose.Schemas.Types.ObjectId(inv_id)});
+
+      if(foundInv){
+        res.status(200).send({invItems: foundInv.items});
+      }else{
+        res.status(404).send({"message": `Inventory with id ${inventoryId} not found!`});
+      }
+
+  } catch (e) {
+      res.status(500).send(e);
+  } finally {
+
+  }
 }
--- routers/inventoryRoutes.js
+++ routers/inventoryRoutes.js
@@ -1,8 +1,10 @@
 const router =  require('express').Router();
 
-const { createInventory } = require('../controllers/inventoryController');
+const { createInventory, addItem } = require('../controllers/inventoryController');
 const { adminAuth } = require('../middleware/auth');
 
 router.post('/createInventory/admin=:admin_id/group=:group:id', adminAuth, createInventory);
+
+router.post('/addItem/inv=:inv_id', addItem);
 
 module.exports = router;
